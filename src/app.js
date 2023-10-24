f658e54458c92834f3ce778867e3eec7d98ae1a4
Rich Kaye <rdkaye444@gmail.com>
2023-10-23 22:07:46 -0400

    express server is working

---
+++ src/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+app.get('/', function (req, res) {
+    res.json({ message: 'Hello from Express!' });
+});
+exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
+// Import the express in typescript file
 import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
+ 
+// Initialize the express engine
+const app: express.Application = express();
+ 
+// Take a port 3000 for running server.
+const port: number = 3000;
+ 
+// Handling '/' Request
+app.get('/', (_req, _res) => {
+    _res.send("TypeScript With Express");
 });
-
-export default app;
+ 
+// Server setup
+app.listen(port, () => {
+    console.log(`TypeScript with Express 
+         http://localhost:${port}/`);
+});
